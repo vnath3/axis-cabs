dbefe007bde53e1513b01b89c3ded48298a4c3fd
shivani <dikondwarshivani22@gmail.com>
2025-08-09 13:30:26 +0530

    build_fix_2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import netlify from '@astrojs/netlify' // <— add
-
-import { fileURLToPath } from 'url'
+import netlify from '@astrojs/netlify'
+import { fileURLToPath } from 'node:url' // ensure 'node:url'
 
 export default defineConfig({
   site: 'https://www.axiscabs.com',
   integrations: [tailwind({ applyBaseStyles: true })],
-  adapter: netlify(),               // <— add (see step 2)
+  adapter: netlify(),
   vite: {
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)), // <— add
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
   },
