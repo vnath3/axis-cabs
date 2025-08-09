19fceae02569cc0348198817a19e398eb4fb77ad
shivani <dikondwarshivani22@gmail.com>
2025-08-09 14:26:45 +0530

    fix_leade_gen

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -3,18 +3,14 @@ import { routes } from '@/data/routes';
 import { packages } from '@/data/packages';
 import { gems } from '@/data/gems';
 
-export const get: APIRoute = () => {
+export const GET: APIRoute = () => {
   const pages = [
     '',
     ...routes.map((r) => `city-to-city/${r.slug}`),
     ...packages.map((p) => `packages/${p.slug}`),
-    ...gems.map((g) => `hidden-gems/${g.slug}`)
+    ...gems.map((g) => `hidden-gems/${g.slug}`),
   ];
-
-  const urls = pages
-    .map((p) => `<url><loc>https://www.axiscabs.com/${p}</loc></url>`)
-    .join('');
-
+  const urls = pages.map((p) => `<url><loc>https://axis-cabs.netlify.app/${p}</loc></url>`).join('');
   const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`;
   return new Response(xml, { headers: { 'Content-Type': 'application/xml' } });
 };
